d8b33f8bbeb0048e0931b9de407f3f52a67af1d7
Giovani Dalmas <giovanidalmas053@gmail.com>
2023-06-25 17:53:03 -0300

    Cabecalho pronto

---
+++ tela principal/js/script.js
@@ -0,0 +1,13 @@
+var images = document.querySelectorAll('.banner img');
+        var currentImageIndex = 0;
+
+        function changeImage(n) {
+            images[currentImageIndex].classList.remove('active');
+            currentImageIndex += n;
+            if (currentImageIndex < 0) {
+                currentImageIndex = images.length - 1;
+            } else if (currentImageIndex >= images.length) {
+                currentImageIndex = 0;
+            }
+            images[currentImageIndex].classList.add('active');
+        }
