ca3bb82271df10896dd64619e6c6fa6bbe55f095
Giovani Dalmas <giovanidalmas053@gmail.com>
2023-07-03 08:38:45 -0300

    so falta menu responsivo

--- views/tela principal/js/script.js
+++ views/tela principal/js/script.js
@@ -1,3 +1,5 @@
+
+/*Banner rotativo*/
 var images = document.querySelectorAll('.banner img');
     var currentImageIndex = 0;
 
@@ -11,3 +13,14 @@ var images = document.querySelectorAll('.banner img');
         }
         images[currentImageIndex].classList.add('active');
     }
+
+/*Scrollagem dos cards*/
+function scrollCardsRight() {
+    const container = document.querySelector('.card-container');
+    container.scrollBy({ left: 600, behavior: 'smooth' });
+}
+
+function scrollCardsLeft() {
+    const container = document.querySelector('.card-container');
+    container.scrollBy({ left: -600, behavior: 'smooth' });
+}
