e71741b08a84bdca5887890697086bba0c1fbccf
Giovani Dalmas <giovanidalmas053@gmail.com>
2023-07-10 08:19:31 -0300

    Quase tela de produto

--- views/tela principal/js/script.js
+++ views/tela principal/js/script.js
@@ -1,18 +1,34 @@
 
 /*Banner rotativo*/
 var images = document.querySelectorAll('.banner img');
-    var currentImageIndex = 0;
+var currentImageIndex = 0;
 
-    function changeImage(n) {
-        images[currentImageIndex].classList.remove('active');
-        currentImageIndex += n;
-        if (currentImageIndex < 0) {
-            currentImageIndex = images.length - 1;
-        } else if (currentImageIndex >= images.length) {
-            currentImageIndex = 0;
-        }
-        images[currentImageIndex].classList.add('active');
+function changeImage(n) {
+    images[currentImageIndex].classList.remove('active');
+    currentImageIndex += n;
+    if (currentImageIndex < 0) {
+        currentImageIndex = images.length - 1;
+    } else if (currentImageIndex >= images.length) {
+        currentImageIndex = 0;
     }
+    images[currentImageIndex].classList.add('active');
+}
+
+function autoChangeImage() {
+    changeImage(1);
+}
+
+var intervalId = setInterval(autoChangeImage, 5000);
+
+var banner = document.querySelector('.banner');
+banner.addEventListener('mouseover', function() {
+    clearInterval(intervalId);
+});
+
+banner.addEventListener('mouseout', function() {
+    intervalId = setInterval(autoChangeImage, 5000);
+});
+
 
 /*Scrollagem dos cards*/
 function scrollCardsRight() {
