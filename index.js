1b446080b37bc2602a2c76752c9b792559c9476e
Giovani Dalmas <giovanidalmas053@gmail.com>
2023-07-12 17:03:35 -0300

    quase linkagens

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const port = 3300;
 
     app.set('view engine', 'ejs');
     app.use(express.static(path.join(__dirname, "public")))
-    app.get('/produto', (req, res) => {res.render('produto.ejs');});
     app.use(express.urlencoded({ extended: true }));
-    app.get('/',(req,res) => {res.render('principal.ejs')});
+
+    app.get('/', (req, res) => {res.render('usuarios/cadastro.ejs')});
+    app.get('/principal', (req, res) => {res.render('principal.ejs')});
+    app.get('/produto', (req, res) => {res.render('produto.ejs')});
 
     app.listen(port, () => { 
         console.log(`Servidor rodando em http://localhost:${port}`);
