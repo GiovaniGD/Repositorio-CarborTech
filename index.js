45ac4579bfedca0c86bca544057124e281378239
Giovani Dalmas <giovanidalmas053@gmail.com>
2023-07-12 13:27:19 -0300

    produto em ejs

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const app = express();
 const port = 3300; 
 
     app.set('view engine', 'ejs');
-    app.use(express.static(path.join(__dirname, "views/css")))
+    app.use(express.static(path.join(__dirname, "public")))
+    app.get('/produto', (req, res) => {res.render('produto.ejs');});
     app.use(express.urlencoded({ extended: true }));
     app.get('/',(req,res) => {res.render('principal.ejs')});
 
