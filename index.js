27192bfe74040c4a54527d70f564893ff399b8a0
Giovani Dalmas <giovanidalmas053@gmail.com>
2023-07-30 11:54:27 -0300

    Com blog

--- index.js
+++ index.js
@@ -9,9 +9,17 @@ const port = 3300;
 
     app.get('/', (req, res) => {res.render('usuarios/cadastro.ejs')});
     app.get('/principal', (req, res) => {res.render('principal.ejs')});
-    app.get('/jabuticabeira', (req, res) => {res.render('jabuticabeira.ejs')});
-    app.get('/araucaria', (req, res) => {res.render('araucaria.ejs')});
+    app.get('/jabuticabeira', (req, res) => {res.render('produtos/jabuticabeira.ejs')});
+    app.get('/araucaria', (req, res) => {res.render('produtos/araucaria.ejs')});
 
-    app.listen(port, () => { 
+    app.get('/inicial', (req, res) => {res.render('carborblog/inicial.ejs')});
+    app.get('/contact', (req, res) => {res.render('carborblog/contact.ejs')});
+    app.get('/about', (req, res) => {res.render('carborblog/about.ejs')});
+    app.get('/o-desmatamento-das-araucarias-ao-longo-da-historia', (req, res) => {res.render('carborblog/posts/post1.ejs')});
+    app.get('/conheca-algumas-especies-nativas-do-rs', (req, res) => {res.render('carborblog/posts/post2.ejs')});
+    app.get('/a-vegetacao-dos-pampas', (req, res) => {res.render('carborblog/posts/post3.ejs')});
+    app.get('/a-vegetacao-da-mata-atlantica', (req, res) => {res.render('carborblog/posts/post4.ejs')});
+
+    app.listen(port, () => {
         console.log(`Servidor rodando em http://localhost:${port}`);
 });
