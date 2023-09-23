7dbb921aac7fe73937bc69f2a5f61fd56996074d
Giovani Dalmas <giovanidalmas053@gmail.com>
2023-09-23 10:48:15 -0300

    mais questoes quiz

--- public/js/questions.js
+++ public/js/questions.js
@@ -21,18 +21,45 @@ export default [
         question: "Qual destas árvores possui mais ocorrência na mata atlântica do Vale do Paranhana?",
         answers: [
             { option: "Chorão", correct: false },
-            { option: "Ipês", correct: false },
+            { option: "Ipê", correct: false },
             { option: "Maricá", correct: true },
-            { option: "Coqueiro", correct: false },
+            { option: "Coqueiro jerivá", correct: false },
         ],
     },
     {
         question: "Quais são as cores das flores encontradas nas espécies de ipê?",
         answers: [
-            { option: "Chorão", correct: false },
-            { option: "Ipês", correct: false },
-            { option: "Maricá", correct: true },
-            { option: "Coqueiro", correct: false },
+            { option: "Amarelo, vermelho, lilás, rosa e verde", correct: false },
+            { option: "Amarelo, rosa, roxo, branco e verde", correct: true },
+            { option: "Amarelo, vermelho, azul, roxo e lilás", correct: false },
+            { option: "Amarelo, verde, lilás, rosa e azul", correct: false },
+        ],
+    },
+    {
+        question: "Qual dessas plantas é nativa do Rio Grande do Sul?",
+        answers: [
+            { option: "Bergamoteira", correct: false },
+            { option: "Eucalipto", correct: false },
+            { option: "Cedro", correct: true },
+            { option: "Laranjeira", correct: false },
+        ],
+    },
+    {
+        question: "Além do Rio Paranhana, qual outro rio importantíssimo para a ecologia do Vale?",
+        answers: [
+            { option: "Rio Taquari", correct: false },
+            { option: "Rio Jacuí", correct: false },
+            { option: "Rio Camaquã", correct: false },
+            { option: "Rio dos Sinos", correct: true},
+        ],
+    },
+    {
+        question: "Qual a porcentagem do restante de Mata de Araucária que ainda há após todo o desmatamento dessa vegetação?",
+        answers: [
+            { option: "Cerca de 10%", correct: false },
+            { option: "Cerca de 3%", correct: true },
+            { option: "Cerca de 1%", correct: false },
+            { option: "Cerca de 7%", correct: false},
         ],
     },
 ]
