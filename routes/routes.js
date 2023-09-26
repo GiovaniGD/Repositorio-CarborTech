24ef747a168fe8717fdcd20066caf021732ed321
Giovani Dalmas <giovanidalmas053@gmail.com>
2023-09-26 10:23:03 -0300

    blog-header arrumado

--- routes/routes.js
+++ routes/routes.js
@@ -10,9 +10,9 @@ routes.get('/quiz', (req, res) => {res.render('servicos/quiz.ejs')});
 routes.get('/inicial', (req, res) => {res.render('blog/inicial.ejs')});
 routes.get('/contact', (req, res) => {res.render('blog/contact.ejs')});
 routes.get('/about', (req, res) => {res.render('blog/about.ejs')});
-routes.get('/o-desmatamento-das-araucarias-ao-longo-da-historia', (req, res) => {res.render('blog/posts/post1.ejs')});
-routes.get('/conheca-algumas-especies-nativas-do-rs', (req, res) => {res.render('blog/posts/post2.ejs')});
-routes.get('/a-vegetacao-dos-pampas', (req, res) => {res.render('blog/posts/post3.ejs')});
-routes.get('/a-vegetacao-da-mata-atlantica', (req, res) => {res.render('blog/posts/post4.ejs')});
+routes.get('/o-desmatamento-das-araucarias-ao-longo-da-historia', (req, res) => {res.render('blog/post1.ejs')});
+routes.get('/conheca-algumas-especies-nativas-do-rs', (req, res) => {res.render('blog/post2.ejs')});
+routes.get('/a-vegetacao-dos-pampas', (req, res) => {res.render('blog/post3.ejs')});
+routes.get('/a-vegetacao-da-mata-atlantica', (req, res) => {res.render('blog/post4.ejs')});
 
 module.exports = routes;
